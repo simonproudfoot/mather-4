ac1cc4db73c91e52dff26832fd011541917c9e2f
simonproudfoot <simonproudfoot1980@gmail.com>
2021-06-04 09:46:52 +0100

    club info

--- pages/club-types.vue
+++ pages/club-types.vue
@@ -3,12 +3,23 @@
     <img :src="require('@/assets/img/background-2.png')" class="background" />
     <div class="pageContent clubSelect z-50">
 
+        <div class="clubSelect__info" style="opacity: 0">
+            <button class="back btn--primary inline-block btn--round align-middle"></button>
+            <h1 class="back inline-block text-primary text-5xl pt-5 mb-5 ml-4 align-middle" @click="selectClub(selected.id)">Back</h1>
+
+            <h1 class="info clubName text-primary text-7xl mt-12 mb-5">{{clubInfo.name}}</h1>
+            <h2 class="info clubUse text-5xl mb-5">Whats is used for?</h2>
+            <p class="info clubText" v-html="clubInfo.text"></p>
+        </div>
+
         <div class="grid grid-cols-4">
             <div class="clubSelect__title col-span-1">
-                <h1 class="text-primary text-7xl">Choose <br> a club</h1>
+                <h1 class="text-primary text-7xl chooseClub">Choose <br> a club</h1>
             </div>
             <div class="col-span-3">
+
                 <div class="grid grid-cols-3">
+
                     <div v-for="(club, i) in $store.getters.clubTypes" class="clubSelect__club p-3" :key="i" @click="selectClub(club.name.replace(' ','_'))" :id="club.name.replace(' ','_')">
                         <img :src="require('@/assets/img/'+club.image)" :id="club.name.replace(' ','_')+'_img'">
                         <h2 class="text-primary text-4xl">{{club.name}}</h2>
@@ -28,43 +39,38 @@ export default {
     data: function () {
         return {
             clubOverlay: false,
-            activeClub: {},
             selected: '',
-            lastLocation: {
-                top: 0,
-                right: 0,
-            }
         }
     },
     methods: {
         selectClub(club) {
-
-            //  const openImage = this.$gsap.to(selected, { right: 575, x: 300, top: 300, duration: 0.4, scale: 2, delay: 0.5 })
-            // if (!this.openImage.reversed()) {
-            //     this.openImage.play();
-            // } else {
-            //     this.openImage.reverse();
-            //     this.$gsap.to(this.selected.children[1], { opacity: 1 })
-            //     this.$gsap.to(this.selected.children[2], { opacity: 1 })
-            // }
-
             this.selected = document.getElementById(club)
             var opacity = null
+            var pEvents = ''
 
             if (this.openImage.reversed()) {
                 opacity = 0
+                pEvents = 'none'
                 this.selected.style.zIndex = '1000'
-                this.$gsap.to(this.selected.children[1], { opacity: 0 })
-                this.$gsap.to(this.selected.children[2], { opacity: 0 })
-                this.$gsap.to('.clubSelect__title', { opacity: 0, x: -50 })
+                this.$gsap.to(this.selected.children[1], { opacity: 0, duration: 0.2, })
+                this.$gsap.to(this.selected.children[2], { opacity: 0, duration: 0.2, delay: 0.2 })
+                this.$gsap.to('.chooseClub', { opacity: 0, x: -50 })
+                this.$gsap.to('.clubSelect__info', { opacity: 1, delay: 0 })
+                this.$gsap.from('.clubSelect__info .info', { x: -50, opacity: 0, stagger: 0.2, delay: 0.2 })
+                this.$gsap.from('.back', { opacity: 0, delay: 1 })
                 this.openImage.play()
             } else {
                 opacity = 1
-                this.selected.style.zIndex = '1'
-                this.$gsap.to(this.selected.children[1], { opacity: 1 })
-                this.$gsap.to(this.selected.children[2], { opacity: 1 })
-                this.$gsap.to('.clubSelect__title', { opacity: 1, x: 0 })
-                this.openImage.reverse()
+                pEvents = 'all'
+
+                this.$gsap.to(this.selected.children[1], { opacity: 1, delay: 0.8 })
+                this.$gsap.to(this.selected.children[2], { opacity: 1, delay: 0.6 })
+                this.$gsap.to('.chooseClub', { opacity: 1, x: 0, delay: 0.5 })
+                this.$gsap.to('.clubSelect__info', { opacity: 0 })
+                this.openImage.reverse().then(() => {
+                    this.selected = ''
+                    this.selected.style.zIndex = '1'
+                })
             }
 
             this.$gsap.to('.background', { opacity: opacity })
@@ -74,13 +80,18 @@ export default {
                 if (children[i].id !== this.selected.id) {
                     console.log(children[i].id)
                     this.$gsap.to('#' + children[i].id, { opacity: opacity })
+                    document.getElementById(children[i].id).style.pointerEvents = pEvents
                 }
             }
         }
     },
     computed: {
         openImage() {
-            return this.$gsap.to(this.selected, { right: 575, x: 300, top: 300, duration: 0.4, scale: 2, }).reverse()
+
+            return this.$gsap.to(this.selected, { right: 700, x: 300, top: 300, duration: 0.6, scale: 2.5, ease: 'inOut' }).reverse()
+        },
+        clubInfo() {
+            return this.selected ? this.$store.getters.clubTypes.find(x => x.name == this.selected.id.replace('_', ' ')) : { name: '', text: '' }
         }
     },
     mounted() {
@@ -105,6 +116,11 @@ export default {
     padding: 6em 8.5em;
     width: $width;
     height: $height;
+
+    &__info {
+        width: 519px;
+        position: absolute;
+    }
 
     &__title {
         padding-top: 280px;
