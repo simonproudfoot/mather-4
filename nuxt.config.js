a891e7330963908bbafd0838a5a493de840eecb5
simonproudfoot <simonproudfoot1980@gmail.com>
2021-06-11 11:18:31 +0100

    Localhost build set

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,20 @@
+var routerBase = '/'
+const projectName = 'ra41'
+
+
+if (process.env.DEPLOY_ENV === 'duckpond') {
+  routerBase = '/ra/'+projectName // Change to whatever the subdirecory is 
+} if (process.env.DEPLOY_ENV === 'onsite') {
+  routerBase = '/'+projectName // Name this whatever the mamp/lamp project filename is called
+}
+
+
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
-  env: {
-    productionMode: 'apache'
+  router: {
+    base: routerBase
   },
 
-  router: {
-  //  base: '/ra/4.1'
-  },
-  //ssr: false,
+  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'mather4',
@@ -24,59 +30,49 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-
-
-  
-
-
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '~/assets/scss/colors.scss',
     '~/assets/scss/type.scss',
     '~/assets/scss/main.scss'
-
   ],
   gsap: {
     extraPlugins: {
       cssRule: true,
     }
   },
-
-
-  
-  babel:{
+  babel: {
     plugins: [
-   
     ]
   },
-
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/mixins',
     '~/plugins/vidle.js'
   ],
-
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
     'nuxt-gsap-module',
   ],
-
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     '@nuxtjs/style-resources',
   ],
-
   styleResources: {
     scss: ['./assets/scss/*.scss']
   },
 
 
+  generate: {
+    dir: projectName,
+    cli: 'If this build is to be used on an apache server (online or local), dont forget to add the .htaccess file'
+  },
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+   
   }
 }
---
+++ ra41/_nuxt/static/1623406561/club-types/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/club-types", {data:[{}],fetch:{},mutations:[]});
